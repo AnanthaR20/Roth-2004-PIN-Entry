09c3995f4db1b770ab50308a234322183d32b7d6
VanR20 <anrao3@wisc.edu>
2024-02-16 23:30:18 +0530

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import PinPad from './KeyPad.svelte';
+
+	let L;
+	let R;
+	let O;
+	let P;
+	function rand_digit(): number {
+		return(Math.floor(Math.random()*10))
+	}
+	function rand_digit_min_i(i: number): number {
+		for (;;) {
+			const j = rand_digit();
+			if (j >= i) {
+				return j;
+			}
+		}
+	}
+	function rand_perm(): Array<number> {
+		let array = new Array<number>(10);
+		for (var i = 0; i < 10; ++i) {
+			array[i] = i;
+		}
+		for (i = 0; i < 9; ++i) {
+			const j = rand_digit_min_i(i);
+			const temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
+	}
+	function gamma(array:Array<number>): Array<Array<number>> {
+		let midpoint = Math.ceil(array.length/2);
+		let L = array.slice(0,midpoint)
+		let R = array.slice(midpoint,array.length);
+		return([L,R])
+	}
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<PinPad />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/KeyPad.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	const color = ["#666666","white"]
+	let key_colors:Uint8Array = new Uint8Array([0,0,0,0,0,1,1,1,1,1]);
+</script>
+
+<div class="keypad" style =
+"--key1_color">
+	<button class="key digit" id="key1" style="background-color:{color[key_colors[0]]}">1</button>
+	<button class="key digit" id="key2" style="background-color:{color[key_colors[1]]}">2</button>
+	<button class="key digit" id="key3" style="background-color:{color[key_colors[2]]}">3</button>
+	<button class="key digit" id="key4" style="background-color:{color[key_colors[3]]}">4</button>
+	<button class="key digit" id="key5" style="background-color:{color[key_colors[4]]}">5</button>
+	<button class="key digit" id="key6" style="background-color:{color[key_colors[5]]}">6</button>
+	<button class="key digit" id="key7" style="background-color:{color[key_colors[6]]}">7</button>
+	<button class="key digit" id="key8" style="background-color:{color[key_colors[7]]}">8</button>
+	<button class="key digit" id="key9" style="background-color:{color[key_colors[8]]}">9</button>
+	<button class="key" id="key_backspace">Backspace</button>
+	<button class="key digit" id="key0" style="background-color:{color[key_colors[9]]}">0</button>
+	<button class="key" id="key_enter">Enter</button>
+</div>
+
+<style>
+	.keypad {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-auto-rows: minmax(100px, auto);
+		border-style: solid;
+	}
+	.key {
+		display: flex;
+		border-style: solid;
+		background: white;
+		align-items: center;
+		justify-content: center;
+	}
+	.digit {
+		font-size: 2em;
+	}
+
+	#key1 {
+		background: var(--key1_color);
+	}
+	#key2 {
+		background: var(--key2_color);
+	}
+	#key3 {
+		background: var(--key3_color);
+	}
+	#key4 {
+		background: var(--key4_color);
+	}
+	#key5 {
+		background: var(--key5_color);
+	}
+	#key6 {
+		background: var(--key6_color);
+	}
+	#key7 {
+		background: var(--key7_color);
+	}
+	#key8 {
+		background: var(--key8_color);
+	}
+	#key9 {
+		background: var(--key9_color);
+	}
+	#key0 {
+		background: var(--key0_color);
+	}
+</style>
